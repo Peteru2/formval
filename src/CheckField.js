97251e094b8ce8d60792a4f8b42d3111fa6d6e14
DevPeter <polalekan526@gmail.com>
2023-09-10 02:24:14 +0100

    little check mistakes adjusted

--- src/CheckField.js
+++ src/CheckField.js
@@ -5,12 +5,18 @@ const CheckField = ({ label, description, price }) => {
 
   const handleDivClick = () => {
     setIsChecked(!isChecked);
-  }
+//     if(price =="+$2/mo"){
+//         console.log(price);
+//     }
+// const check = document.querySelector(".check").value
+// console.log(check)
+//   }
+// value={isChecked?"2":"3"}
 
     return (
         <>
-                <div className="flex border-2 border-blue-900 p-4 rounded-md mt-8" onClick={handleDivClick}>
-            <input type="checkbox" checked={isChecked} onChange={() => {}} />
+                <div className={isChecked?"flex border-2 border-blue-900 p-4 rounded-md mt-8 cursor-pointer":"flex border-2 p-4 rounded-md mt-8 cursor-pointer"} onClick={handleDivClick}>
+            <input type="checkbox" checked={isChecked} value={isChecked?"2":"3"} className="check" name="checkbox" onChange={() => {}} />
             <div className="ml-3">
                 <p className="text-blue-900 text-sm">{label}</p>
                 <p className="text-gray-300 text-xs">{description}</p>
