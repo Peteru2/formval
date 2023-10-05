ee00dc7b722e93af4284ffe97eab6229f22bda66
DevPeter <polalekan526@gmail.com>
2023-10-05 20:26:46 +0100

    npm run build included

--- babel.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    presets: ["@babel/preset-react"],
-    plugins: ["@babel/plugin-syntax-jsx"]
-
-  }
--- src/landing.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import LandingPage from "./Landing"
-
-test('renders LandingPage', () => {
-    render(<MyComponent />);
-    // const element = screen.getByText('Hello, World!');
-    // expect(element).toBeInTheDocument();
-  });
